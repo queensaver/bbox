5592fefe680dd2437d71617950ea7e04fc74e5df
Wolfgang Hennerbichler <wogri@wogri.com>
2020-08-19 22:34:34 +0200

    initial prototype works, hooray.

--- server/go.mod
+++ server/go.mod
@@ -1,1 +1,3 @@
 module github.com/wogri/bbox/server
+
+require github.com/wogri/bbox/thingspeak_client v0.0.0-20200819201122-559f8326d9a6
--- server/main.go
+++ server/main.go
@@ -1,29 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"log"
-  "net/http"
 	"github.com/wogri/bbox/thingspeak_client"
+	"log"
+	"net/http"
 )
 
-var httpServerPort = flag.Int("http_server_port", "8333", "HTTP server port")
+var httpServerPort = flag.String("http_server_port", "8333", "HTTP server port")
 var debug = flag.Bool("debug", false, "debug mode")
 var thingspeakKey = flag.String("thingspeak_api_key", "48PCU5CAQ0BSP4CL", "API key for Thingspeak")
 var thingspeakActive = flag.Bool("thingspeak", false, "Activate thingspeak API if set to true")
 
-func main() {
-	flag.Parse()
+type Scale struct {
+	Weight float64
+	BBoxID string //BBoxID is usually the Mac address of the raspberry pi in the bHive.
+}
 
-	thing := thingspeak_client.NewChannelWriter(*thingspeakKey)
-	thing.AddField(1, medianWeight)
+func thingSpeakUpdate(weight float64) error {
 	if *thingspeakActive {
+		thing := thingspeak_client.NewChannelWriter(*thingspeakKey)
+		thing.AddField(1, weight)
 		if *debug {
 			log.Println("uploading data to Thingspeak...")
 		}
-		_, err = thing.Update()
+		_, err := thing.Update()
 		if err != nil {
-			log.Println("ThingSpeak error:", err)
+			return err
 		}
 	}
+	return nil
+
+}
+
+func scaleHandler(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var scale Scale
+	err := decoder.Decode(&scale)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(scale.Weight)
+}
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/scale", scaleHandler)
+	log.Fatal(http.ListenAndServe(":"+*httpServerPort, nil))
 }
