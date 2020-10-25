7de00c92d45fec54533969f7c6e00eb7848a4e76
Wolfgang Hennerbichler <wogri@wogri.com>
2020-10-25 20:00:55 +0100

    added logger

---
+++ packages/logger/logger.go
@@ -0,0 +1,43 @@
+package logging
+
+import (
+    "github.com/Sirupsen/logrus"
+)
+
+var (
+    log *logrus.Logger
+)
+
+func init() {
+    log = logrus.New()
+    log.Formatter = &logrus.JSONFormatter{}
+
+    log.SetReportCaller(true)
+}
+
+func Info(format string, v ...interface{}) {
+    log.Infof(format, v...)
+}
+
+func Warn(format string, v ...interface{}) {
+    log.Warnf(format, v...)
+}
+
+func Error(format string, v ...interface{}) {
+    log.Errorf(format, v...)
+}
+
+var (
+
+    // ConfigError ...
+    ConfigError = "%v type=config.error"
+
+    // HTTPError ...
+    HTTPError = "%v type=http.error"
+
+    // HTTPWarn ...
+    HTTPWarn = "%v type=http.warn"
+
+    // HTTPInfo ...
+    HTTPInfo = "%v type=http.info"
+)
