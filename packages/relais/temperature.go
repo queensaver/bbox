ff19e949027be87661e9a6ea5ac18cab1f8c75e8
Wolfgang Hennerbichler <wogri@wogri.com>
2021-04-07 22:35:52 +0200

    temperature package is ready to start

---
+++ packages/relais/temperature.go
@@ -0,0 +1,21 @@
+package temperature
+
+import (
+  "github.com/stianeikeland/go-rpio/v4"
+)
+
+func OpenAllRelais(gpios []int) error {
+  err := rpio.Open()
+  if err != nil {
+    return err
+  }
+  defer rpio.Close()
+
+  for _, gpio := range gpios {
+    pin := rpio.Pin(gpio)
+    pin.Output()       // Output mode
+    pin.High()
+    // pin.Low()
+  }
+  return nil
+}
