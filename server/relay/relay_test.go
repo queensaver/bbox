50ed3c97bc30f70fb4ef8a703eb50974b7743e33
Wolfgang Hennerbichler <wogri@wogri.com>
2021-04-10 07:15:33 +0200

    tests, config

--- server/relay/relay_test.go
+++ server/relay/relay_test.go
@@ -63,3 +63,34 @@ func TestSingleRelay(t *testing.T) {
   }
 }
 
+func TestThreeRelays(t *testing.T) {
+  r := RelayModule{}
+  relaySwitches := []Switcher{&SwitchMock{Gpio: 16}, &SwitchMock{Gpio: 15}, &SwitchMock{Gpio: 14}}
+  err := r.Initialize(relaySwitches)
+  if err != nil {
+    t.Fatalf("relay couldn't be initialized: %s", err)
+    return
+  }
+  for i := range []int{1,2,3} {
+    if r.Switches[i].GetState() == true {
+      t.Errorf("Switch is supposed to be turned off!")
+    }
+  }
+  /*
+  done, err := r.ActivateNextBHive()
+  if done == true {
+    t.Errorf("One switch shouldn't return success here.")
+  }
+  if r.Switches[0].GetState() == false {
+    t.Errorf("Switch should be on!")
+  }
+  done, err = r.ActivateNextBHive()
+  if done == false {
+    t.Errorf("We should be done by now!")
+  }
+  if r.Switches[0].GetState() == true {
+    t.Errorf("Switch should be off!")
+  }
+  */
+}
+
