e8dc384690cb8085d4cae7e75ceed14819864a05
Wolfgang Hennerbichler <wogri@github.com>
2023-04-27 14:29:06 +0200

    disable certificate checks because our cert is so cheap

--- server/buffer/buffer.go
+++ server/buffer/buffer.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"crypto/tls"
 
 	"github.com/queensaver/bbox/server/scheduler"
 	"github.com/queensaver/packages/logger"
@@ -266,6 +267,8 @@ func (f *FileSurgeon) RemountRW() error {
 	return nil
 }
 func (h HttpPostClient) PostData(request string, data SensorValuer) error {
+  http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
   uuid := data.GetUUID()
   data.ClearUUID()
 	j, err := json.Marshal(data)
