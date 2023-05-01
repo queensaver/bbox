3570121ed46a73f3bcd7255c20dad00387441c58
Wolfgang Hennerbichler <wogri@wogri.com>
2023-05-01 15:30:27 +0000

    fix capture script

--- ansible/roles/socket/files/capture.sh
+++ ansible/roles/socket/files/capture.sh
@@ -5,7 +5,7 @@ sleep 1
 D=/home/pi/scan
 libcamera-jpeg --brightness=0.1 -o bOS/$D.jpg
 python3 /home/pi/hw.py --debug scan do light 0
-mac=$(ip link show dev vnet15 | grep link/ether | awk '{ print $2 }' | tr -d \:)
+mac=$(ip link show dev eth0 | grep link/ether | awk '{ print $2 }' | tr -d \:)
 curl -v -F bhiveId=$mac -F epoch=$D -F scan=@bOS/$D.jpg http://localhost/varroa && \
   rm bOS/$D.jpg
 python3 /home/pi/hw.py --debug scan do move 100 6
