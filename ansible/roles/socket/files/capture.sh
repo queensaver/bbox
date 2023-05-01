a87d58805d699e22245b3101b6add8ac81cfb3d4
Wolfgang Hennerbichler <wogri@wogri.com>
2023-05-01 16:01:34 +0000

    even more simple now

--- ansible/roles/socket/files/capture.sh
+++ ansible/roles/socket/files/capture.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 python3 /home/pi/hw.py --debug scan do light 1
 sleep 1
 # D=$(date +%s)
@@ -6,6 +6,5 @@ D=scan
 libcamera-jpeg --brightness=0.1 -o bOS/$D.jpg
 python3 /home/pi/hw.py --debug scan do light 0
 mac=$(ip link show dev eth0 | grep link/ether | awk '{ print $2 }' | tr -d \:)
-curl -v -F bhiveId=$mac -F epoch=$D -F scan=@bOS/$D.jpg http://localhost/varroa && \
-  rm bOS/$D.jpg
+curl -v -F bhiveId=$mac -F epoch=$D -F scan=@bOS/$D.jpg http://localhost/varroa 
 python3 /home/pi/hw.py --debug scan do move 100 6
