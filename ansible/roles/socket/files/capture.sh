25d4bd7f4aa5b33dc45354e660cf251aadf15601
Wolfgang Hennerbichler <wogri@wogri.com>
2023-05-01 15:00:07 +0200

    fix path

--- ansible/roles/socket/files/capture.sh
+++ ansible/roles/socket/files/capture.sh
@@ -3,8 +3,9 @@ python3 /home/pi/hw.py --debug scan do light 1
 sleep 1
 # D=$(date +%s)
 D=/home/pi/scan
-libcamera-jpeg --brightness=0.1 -o $D.jpg
+libcamera-jpeg --brightness=0.1 -o bOS/$D.jpg
 python3 /home/pi/hw.py --debug scan do light 0
 mac=$(ip link show dev vnet15 | grep link/ether | awk '{ print $2 }' | tr -d \:)
-curl -v -F bhiveId=$mac -F epoch=$D -F scan=@$D.jpg http://localhost/varroa
+curl -v -F bhiveId=$mac -F epoch=$D -F scan=@bOS/$D.jpg http://localhost/varroa && \
+  rm bOS/$D.jpg
 python3 /home/pi/hw.py --debug scan do move 100 6
