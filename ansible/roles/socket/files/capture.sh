12690c5dd40d75bf43a24553bea35372985e2749
Wolfgang Hennerbichler <wogri@wogri.com>
2023-05-03 07:46:54 +0200

    wlan

--- ansible/roles/socket/files/capture.sh
+++ ansible/roles/socket/files/capture.sh
@@ -7,6 +7,6 @@ FILENAME=/home/pi/bOS/scan.jpg
 D=$(date +%s)
 libcamera-jpeg --brightness=0.1 -o $FILENAME
 python3 /home/pi/hw.py --debug scan do light 0
-mac=$(ip link show dev eth0 | grep link/ether | awk '{ print $2 }' | tr -d \:)
+mac=$(ip link show dev wlan0 | grep link/ether | awk '{ print $2 }' | tr -d \:)
 curl -v -F bhiveId=$mac -F epoch=$D -F scan=@$FILENAME http://localhost/varroa 
 python3 /home/pi/hw.py --debug scan do move 100 6
