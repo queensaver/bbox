51dd13e3cfbfb8af632acc0bcaaa24644eb90e4a
Wolfgang Hennerbichler <wogri@wogri.com>
2023-06-05 11:17:08 +0200

    update mover

--- ansible/roles/socket/files/capture.sh
+++ ansible/roles/socket/files/capture.sh
@@ -9,4 +9,4 @@ libcamera-jpeg --brightness=0.1 -o $FILENAME
 python3 /home/pi/hw.py --debug scan do light 0
 mac=$(ip link show dev wlan0 | grep link/ether | awk '{ print $2 }' | tr -d \:)
 curl -v -F bhiveId=$mac -F epoch=$D -F scan=@$FILENAME http://localhost/varroa 
-python3 /home/pi/hw.py --debug scan do move 100 6
+python3 /home/pi/hw.py --debug scan do move 440 6
